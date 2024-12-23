c6a50f8500bf84cb5c89036a23100909becb6bda
BooleanCube <samarthupadhya2309@gmail.com>
2024-12-23 00:37:40 -0500

    gradient and glow styling

--- frontend/src/components/ui/Hero.tsx
+++ frontend/src/components/ui/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       className="min-h-screen flex flex-col justify-center items-start container mx-auto px-4"
     >
       <p className="text-secondary">Hi, my name is</p>
-      <h1 className="text-4xl md:text-6xl font-bold mb-4 text-primary">BooleanCube.</h1>
+      <h1 className="text-4xl md:text-6xl font-bold mb-4">BooleanCube.</h1>
       <h2 className="text-3xl md:text-5xl font-bold text-text">I build things.</h2>
       <p className="mt-6 max-w-2xl">
         I'm a software engineer. yada yada yada...
